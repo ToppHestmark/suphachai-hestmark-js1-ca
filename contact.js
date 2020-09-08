a9417476557c82ef48e66661488bc4aac735c06a
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-08 10:48:11 +0200

    Finished off form validation in js

--- contact.js
+++ contact.js
@@ -1,20 +1,25 @@
 const form = document.querySelector("#contactForm");
-const name = document.querySelector("#name");
-const nameError = document.querySelector("#nameError");
 const subject = document.querySelector("#subject");
 const subjectError = document.querySelector("#subjectError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
 
-
+form.addEventListener("submit", validateForm)
 
 function validateForm() {
   event.preventDefault();
-
 
   subject.value.trim().length >= 10 
   ? subjectError.style.display = "none"
   : subjectError.style.display = "block"
 
-  console.log(subject.value.trim().length)
+  address.value.trim().length >= 25 
+  ? addressError.style.display = "none"
+  : addressError.style.display = "block"
+
+  validateEmail(email.value)
+  ? emailError.style.display = "none"
+  : emailError.style.display = "block"
 }
 
-form.addEventListener("submit", validateForm)
+const validateEmail = email =>  /\S+@\S+\.\S+/.test(email);
