cca2c0d8be810213e2ffbb8fb78f58c1bcc74cd3
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-08 12:51:58 +0200

    Added success form validation message

--- contact.js
+++ contact.js
@@ -3,23 +3,30 @@ const subject = document.querySelector("#subject");
 const subjectError = document.querySelector("#subjectError");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
+const passedValidation = document.querySelector("#passedValidation")
 
 form.addEventListener("submit", validateForm)
 
 function validateForm() {
   event.preventDefault();
 
-  subject.value.trim().length >= 10 
-  ? subjectError.style.display = "none"
+  subject.value.trim().length >= 10
+  ? subjectError.style.display = "none" 
   : subjectError.style.display = "block"
 
-  address.value.trim().length >= 25 
+  address.value.trim().length >= 25
   ? addressError.style.display = "none"
   : addressError.style.display = "block"
 
   validateEmail(email.value)
   ? emailError.style.display = "none"
   : emailError.style.display = "block"
+
+  subject.value.trim().length >= 10 === true
+  && address.value.trim().length >= 25 === true
+  && validateEmail(email.value) === true
+  ? passedValidation.style.display = "block"
+  : passedValidation.style.display = "none"
 }
 
 const validateEmail = email =>  /\S+@\S+\.\S+/.test(email);
