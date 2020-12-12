4d4f0ec8b576bb78ed924f5a95094eb236acfa8a
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-12-12 20:56:19 +0100

    Navbar under construction

--- src/app.js
+++ src/app.js
@@ -70,3 +70,42 @@ function createPastLaunchesHtml(pastLaunchesResults) {
   </a>`;
   });
 }
+
+// ------------- Navbar ---------------
+
+const body = document.querySelector("body");
+const nav = document.querySelector("nav");
+
+const navbarSlideFromTheSide = () => {
+  const burger = document.querySelector(".nav__burger");
+  const navLinksList = document.querySelector(".nav__links");
+  const navLinks = document.querySelectorAll(".nav__links li");
+
+  burger.addEventListener("click", () => {
+    body.classList.toggle("body--preventScrolling");
+    navLinksList.classList.toggle("nav--active");
+
+    navLinks.forEach((link, index) => {
+      const easeSpeed = index / 7 + 0.2;
+
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `nav--linksFade 0.5s ease forwards ${easeSpeed}s`;
+      }
+    });
+
+    burger.classList.toggle("nav--burgerToggle");
+  });
+};
+navbarSlideFromTheSide();
+
+window.onscroll = () => {
+  const scrollingPosition = Math.ceil(window.scrollY);
+
+  if (scrollingPosition > 100) {
+    nav.classList.add("nav--fill");
+  } else {
+    nav.classList.remove("nav--fill");
+  }
+};
